b0f465e9a6c083362c9e252be715440b958caa2e
ravikanthtadikonda1979 <ravikanth.x.tadikonda@outlook.com>
2018-05-09 09:38:41 +0530

    Added new project product search

---
+++ src/main/java/com/amazon/productsearch/package-info.java
@@ -0,0 +1,7 @@
+@XmlSchema(
+        namespace = "http://webservices.amazon.com/AWSECommerceService/2013-08-01",
+        elementFormDefault = XmlNsForm.QUALIFIED)
+package com.amazon.productsearch;
+
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
